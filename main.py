39df4b5918516cb01573ef0bc6a3a65db3352339
alekshandra <alekshandraarjun@yahoo.co.uk>
2022-02-21 16:07:49 +0000

    Increased exploration, added prints and additional plots

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import math
 import torch
 import matplotlib.pyplot as plt
-
+import random
 from env import Environment
 from classes import SoftActorCritic
 
@@ -11,34 +12,45 @@ action_dim = env.action_space.shape[0]
 state_dim = env.observation_space.shape[0]
 hidden_dim = 256
 
-sac = SoftActorCritic(action_dim, state_dim, hidden_dim)
+sac = SoftActorCritic(action_dim, state_dim, hidden_dim, replay_buffer_size=25000)
 
-max_frames = 1500
+max_frames = 100000
 max_steps = 300
 frame_idx = 0
 rewards = []
 batch_size = 128
+exploration_start = 0.95
+exploration_end = 0.001
 
 while frame_idx < max_frames:
     state = env.reset()
     episode_reward = 0
-    print(frame_idx)
+    print("\n\n\n\n\n----------- Frame: ", frame_idx)
 
     for step in range(max_steps):
-        if frame_idx > 1000:
-            action, _, _, _, _ = sac.policy_net.get_action(torch.FloatTensor(state).unsqueeze(0))
-            action = action.detach()[0]
-            next_state, reward, done, _ = env.step(action.numpy())
+        print("\n\n------ Taking A Bold Step -----")
+        # state[0:4] /= 180
+        # state[5:9] /= 180
+        if frame_idx > 2000:
+            if random.random() > exploration_end+exploration_start*math.exp(0.0001*(2000-frame_idx)):
+                print("... with intention!")
+                action, _, _, _, _ = sac.policy_net.get_action(torch.FloatTensor(state).unsqueeze(0))
+                action = action.detach()[0]
+                next_state, reward, done, _ = env.step(action.numpy())
+            else:
+                print("... randomly...")
+                action = env.action_space.sample()
+                next_state, reward, done, _ = env.step(action)
         else:
             action = env.action_space.sample()
             next_state, reward, done, _ = env.step(action)
 
-        # print(action)
-        # print(next_state, reward, done)
+        # print("Main Script Action: ", action)
+        # print("Main Script Next State, Reward, Done: ", next_state, reward, done)
         sac.replay_buffer.push(state, action, reward, next_state, done)
 
-        state = next_state
-        # print(state, reward)
+        state = next_state.copy()
+        # print("Main Script State, Reward: ", state, reward)
         episode_reward += reward
         frame_idx += 1
 
@@ -46,8 +58,11 @@ while frame_idx < max_frames:
             sac.update(batch_size)
 
         if frame_idx % 10000 == 0:
-            plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
-            plt.plot(rewards)
+            fig, axs = plt.subplots(1, 2)
+            fig.suptitle('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+            axs[0].plot(rewards)
+            axs[1].plot(rewards[-1000:])
+            # plt.plot(rewards)
             plt.show()
 
         if done:
@@ -61,16 +76,16 @@ state = env.reset()
 cum_reward = 0
 
 plt.ion()
-
+print("\n\n\n\n-------------- Time To See How It Gets On! ------------")
 for t in range(500):
     print(t)
     plt.clf()
     env.render()
     action, _, _, _, _ = sac.policy_net.get_action(torch.FloatTensor(state).unsqueeze(0))
     action = action.detach()[0]
-    state, reward, done, info = env.step(action)
+    state, reward, done, info = env.step(action.numpy())
     cum_reward += reward
     if done:
         break
 
-    plt.pause(10.1)
+    plt.pause(0.1)
